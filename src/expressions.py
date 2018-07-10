fb0e6a109ed607e1bd6987ff88625017d5788133
Thomas Mailund <mailund@birc.au.dk>
2018-07-10 07:20:53 +0200

    begining infix parser

--- src/expressions.py
+++ src/expressions.py
@@ -36,7 +36,6 @@ operator_table = {
 }
 
 def evaluate_rec(expr, i):
-	print(expr[i])
 	if expr[i] in ('+', '-', '*', '/'):
 		op = operator_table[expr[i]]
 		a, i = evaluate_rec(expr, i + 1)
@@ -50,3 +49,29 @@ print(evaluate(prefix_expr))
 
 
 ## Infix
+def tokenise(expr):
+	for op in ("(", ")", "+", "-", "*", "/"):
+		expr = expr.replace(op, " " + op + " ")
+	return expr.split()
+
+
+
+def evaluate(expr):
+	expr_list = tokenise(expr)
+	val, idx = evaluate_rec(expr_list, 0)
+	return val
+
+def evaluate_rec(expr, i):
+	if expr[i] == '(':
+		a, i = evaluate_rec(expr, i + 1)
+		op = operator_table[expr[i]]
+		b, i = evaluate_rec(expr, i + 1)
+		return op(a, b), i + 1 # + 1 to get past the ')'
+	else:
+		return int(expr[i]), i + 1
+
+print("infix")
+print(evaluate('12'))
+print(evaluate('(2 + 3)'))
+print(evaluate('((2 + 2) * 3)'))
+
